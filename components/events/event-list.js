ee25922ef521d15fb7aa40669850beb9c8ccfe80
Bishko <yurik322412@gmail.com>
2022-01-21 16:58:45 +0200

    Adding Regular React Components

---
+++ components/events/event-list.js
@@ -0,0 +1,11 @@
+import EventItem from "./event-item";
+
+const EventList = (props) => {
+  const { items } = props;
+
+  return (
+    <ul>{items.map(event => <EventItem />)}</ul>
+  );
+}
+
+export default EventList;
