df458111a1e570c91dfa92d425fda085192aea6f
Bishko <yurik322412@gmail.com>
2022-01-21 19:01:12 +0200

    Styling Components

--- components/events/event-list/index.js
+++ components/events/event-list/index.js
@@ -1,10 +1,12 @@
-import EventItem from './event-item';
+import EventItem from '../event-item';
+import classes from './styles.module.css';
+
 
 const EventList = (props) => {
   const { items } = props;
 
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map((event) => {
         const { title, image, location, date, id } = event;
         return (
