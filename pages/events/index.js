dac00ab5eb2cb948be271fbda779406b890f0fe6
Bishko <yurik322412@gmail.com>
2022-01-22 14:25:34 +0200

    Working on the "All Events" Page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,12 @@
-const AllEventsPage = () => {
-  return (
-    <div>
-      <h1>All Events Page</h1>
-    </div>
-  );
-}
+import EventList from '../../components/events/event-list';
+import { getAllEvents } from '../../dummy-data';
+
+const events = getAllEvents();
+
+const AllEventsPage = () => (
+  <div>
+    <EventList items={events} />
+  </div>
+);
 
 export default AllEventsPage;
