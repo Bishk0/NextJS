16a9b65654b1fc95d8ea5e9b0bba691a95ea91ae
Yurii Bishko <yurii.bishko@indeema.com>
2022-02-23 15:57:36 +0200

    Adding the 'All Posts' Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,33 @@
-const AllPostPage = (props) => {
+import AllPosts from '../../components/posts/all-posts';
 
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started',
+    title: 'Getting Started',
+    image: 'getting-started.jpg',
+    excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+    date: '2022-02-10'
+  },
+  {
+    slug: 'getting-started2',
+    title: 'Getting Started2',
+    image: 'getting-started.jpg',
+    excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+    date: '2021-02-10'
+  },
+  {
+    slug: 'getting-started3',
+    title: 'Getting Started3',
+    image: 'getting-started.jpg',
+    excerpt: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+    date: '2022-03-10'
+  }
+];
+
+const AllPostPage = (props) => {
+  return (
+    <AllPosts posts={DUMMY_POSTS} />
+  )
 }
 
 export default AllPostPage;
