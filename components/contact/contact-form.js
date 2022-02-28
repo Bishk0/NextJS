141aa5174c29f0c15e047dc5af3aa58d9c96fce3
Yurii Bishko <yurii.bishko@indeema.com>
2022-02-28 17:51:36 +0200

    Storing Messages With MongoDB In A Database

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,23 +1,64 @@
+import { useState } from 'react';
 import classes from './contact-form.module.css';
 
 const ContactForm = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredMessage, setEnteredMessage] = useState('');
+
+  const sendMessageHandler = (e) => {
+    e.preventDefault();
+
+    fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  };
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" name="email" required />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={enteredEmail}
+              onChange={(e) => setEnteredEmail(e.target.value)}
+              required
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="name" id="name" name="name" required />
+            <input
+              type="name"
+              id="name"
+              name="name"
+              value={enteredName}
+              onChange={(e) => setEnteredName(e.target.value)}
+              required
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea name="message" id="message" rows="5"></textarea>
+          <textarea
+            name="message"
+            id="message"
+            rows="5"
+            value={enteredMessage}
+            onChange={(e) => setEnteredMessage(e.target.value)}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -21,9 +23,34 @@ const handler = (req, res) => {
       message
     };
 
-    console.log(newMessage);
+    let client;
 
-    res.status(201).json({ message: 'Successfully stored message.', message: newMessage });
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://Bishko:6969@cluster0.35ldd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+      );
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong.' });
+
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong.' });
+
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: 'Successfully stored message.', message: newMessage });
   }
 };
 
